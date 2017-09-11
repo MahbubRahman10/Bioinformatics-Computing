90a13649a2a81a0f6928d1229ba7133d089d29f4
Mahbub Rahman <Mahbub.shuvo10@gmail.com>
2017-09-11 17:49:28 +0600

    Add files via upload
    Nucleotide Count

---
+++ Nucleotide Count.py
@@ -0,0 +1,19 @@
+def nucleotide_count(dna):
+    A,T,C,G=0,0,0,0
+    
+    for n in dna:
+        if n=='A':
+            A+=1
+        elif n=='T':
+            T+=1
+        elif n=='C':
+            C+=1
+        elif n=='G':
+            G+=1
+            
+    return A,T,C,G
+
+dna="ATTCGAACCC"
+a,t,c,g=nucleotide_count(dna)
+
+print("A:", a, "T:", t,"C:", c, "G:", g)
